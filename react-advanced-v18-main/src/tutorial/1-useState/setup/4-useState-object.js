a8e88024746a6f7c8f25ef98f980ebf26dea47f4
ShannonGW <ShannonWilliams93@gmail.com>
2022-09-22 13:47:51 -0400

    useState object change message button

--- react-advanced-v18-main/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced-v18-main/src/tutorial/1-useState/setup/4-useState-object.js
@@ -6,11 +6,19 @@ const UseStateObject = () => {
     age: 24,
     message: "random message",
   });
+
+  const changeMessage = () => {
+    setPerson({ ...person, message: "Hello!" });
+    //use spread operator so that name and age dont change, just message
+  };
   return (
     <>
       <h3> {person.name}</h3>
       <h3> {person.age}</h3>
       <h3> {person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        Change Message
+      </button>
     </>
   );
 };
