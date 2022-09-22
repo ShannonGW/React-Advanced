bde6dd5f578ae6bd1456148f3aded7e386d51470
ShannonGW <ShannonWilliams93@gmail.com>
2022-09-22 13:29:30 -0400

    useState object practice

--- react-advanced-v18-main/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced-v18-main/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Shannon",
+    age: 24,
+    message: "random message",
+  });
+  return (
+    <>
+      <h3> {person.name}</h3>
+      <h3> {person.age}</h3>
+      <h3> {person.message}</h3>
+    </>
+  );
 };
 
 export default UseStateObject;
