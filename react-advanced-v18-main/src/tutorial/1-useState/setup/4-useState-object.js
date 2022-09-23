96a3b3cf4bfdfb836a31cace16dd8ad937e29287
ShannonGW <ShannonWilliams93@gmail.com>
2022-09-23 13:01:50 -0400

    useState instead of large object

--- react-advanced-v18-main/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced-v18-main/src/tutorial/1-useState/setup/4-useState-object.js
@@ -2,20 +2,26 @@ import React, { useState } from "react";
 
 const UseStateObject = () => {
   const [person, setPerson] = useState({
-    name: "Shannon",
-    age: 24,
-    message: "random message",
+    // name: "Shannon",
+    // age: 28,
+    // message: "random message",
   });
 
+  const [name, setName] = useState("Shannon");
+  const [age, setAge] = useState(28);
+  const [message, setMessage] = useState("random message");
+
   const changeMessage = () => {
-    setPerson({ ...person, message: "Hello!" });
+    // setPerson({ ...person, message: "Hello!" });
     //use spread operator so that name and age dont change, just message
+    setMessage("Hello world!");
   };
+
   return (
     <>
-      <h3> {person.name}</h3>
-      <h3> {person.age}</h3>
-      <h3> {person.message}</h3>
+      <h3> {name}</h3>
+      <h3> {age}</h3>
+      <h3> {message}</h3>
       <button className="btn" onClick={changeMessage}>
         Change Message
       </button>
